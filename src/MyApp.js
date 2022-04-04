40d108f81e2b8baeddb0e9fd542d2d03ccb00612
Yun Ei Hlaing <yuneihlaing@gmail.com>
2022-04-03 17:06:11 -0700

    Second commit

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,31 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
-const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor",
-  },
-  {
-    name: "Mac",
-    job: "Bouncer",
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress",
-  },
-  {
-    name: "Dennis",
-    job: "Bartender",
-  },
-];
-
 function MyApp() {
+  const characters = useState([
+    {
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ]);
+
   return (
     <div className="container">
-      <Table characterData={characters} />
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
     </div>
   );
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
 }
 
 export default MyApp;
